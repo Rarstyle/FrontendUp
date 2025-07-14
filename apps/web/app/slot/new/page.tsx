3814f67c9142a00af6072e019b0dfb6d119515b4
Rail <railsh116@mail.ru>
2025-07-14 15:22:25 +0300

    Deployment error fixed

--- apps/web/app/slot/new/page.tsx
+++ apps/web/app/slot/new/page.tsx
@@ -2,11 +2,6 @@
 
 import SlotForm from '../../../components/SlotForm';
 
-export const metadata = {
-  title: 'Создать новый A/B-тест – NeuroAd',
-  description: 'Создайте новый A/B-тест для оптимизации рекламных креативов.',
-};
-
 export default function NewSlotPage() {
   return <SlotForm />;
 }
